25398e207bd19094e747334369af644194aa41ba
LaupWing <loc_strict@hotmail.com>
2018-12-24 16:39:32 +0100

    first commit the tutorial

---
+++ server/routes/api/posts.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+ // Get Posts
+router.get('/', async (req, res)=>{
+  const posts = await loadPostCollection();
+  res.send(await posts.find({}).toArray());
+})
+
+ // Add Postst
+router.post('/', async (req, res)=>{
+  const posts = await loadPostCollection();
+  await posts.insertOne({
+    text: req.body.text,
+    createdAt: new Date()
+  });
+  res.status(201).send();
+
+});
+
+
+ // Delete posts
+ router.delete('/:id', async(req,res)=>{
+    const posts = await loadPostCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)})
+    res.status(200).send();
+ })
+
+async function loadPostCollection() {
+  const client = await mongodb.MongoClient.connect('mongodb://abc123:abc123@ds243344.mlab.com:43344/vue_express',{
+    useNewUrlParser: true
+  })
+
+  return client.db('vue_express').collection('posts')
+}
+
+module.exports = router;
